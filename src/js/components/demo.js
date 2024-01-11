8d792c6001adb8ca452afb8297bab15b2f662457
Rodolfo Reynoso <rodolfo.reynoso@icloud.com>
2024-01-11 16:34:50 -0500

    JP-16 Change Paddle Movement from position to transform
    The paddle movement was changed from position relative to
    translateY because the former was causing a bug when the first movement
    occured.

--- src/js/components/demo.js
+++ src/js/components/demo.js
@@ -10,25 +10,25 @@ window.addEventListener("keydown",
      
       // Player 1
       if(event.code == 'KeyW') {
-        player1Position +=playerMovementSpeed
-        player1.style.bottom = player1Position + 'px'
+        player1Position -=playerMovementSpeed
+        player1.style.transform = `translateY(${player1Position}px)`
       }
       if(event.code == 'KeyS') {
-        player1Position -=playerMovementSpeed
-        player1.style.bottom = player1Position + 'px'
+        player1Position +=playerMovementSpeed
+        player1.style.transform = `translateY(${player1Position}px)`
 
       }
 
       // Player 2
       if(event.code == 'ArrowUp') {
 
-        player2Position +=playerMovementSpeed
-        player2.style.bottom = player2Position + 'px'
+        player2Position -=playerMovementSpeed
+        player2.style.transform = `translateY(${player2Position}px)`
       }
       if(event.code == 'ArrowDown') {
 
-        player2Position -=playerMovementSpeed
-        player2.style.bottom = player2Position + 'px'
+        player2Position +=playerMovementSpeed
+        player2.style.transform = `translateY(${player2Position}px)`
       }
 
     }
