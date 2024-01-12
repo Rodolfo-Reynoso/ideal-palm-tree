ad9d8d25c2ba2e0c93c37d59a564a3fd73c1de8a
Rodolfo Reynoso <rodolfo.reynoso@icloud.com>
2024-01-11 23:47:45 -0500

    Add ball

--- src/js/components/demo.js
+++ src/js/components/demo.js
@@ -5,6 +5,25 @@ let player1 = document.getElementById("controllerPlayer1")
 let player2 = document.getElementById("controllerPlayer2")
 
 
+function isColliding(element1, element2) {
+  const rect1 = element1.getBoundingClientRect();
+  const rect2 = element2.getBoundingClientRect();
+
+  return (
+    rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y
+  );
+}
+
+if(isColliding(player1, player2)) {
+  console.log("Elements are colliding")
+} else {
+  console.log("Elements are not colliding")
+}
+ 
+
 window.addEventListener("keydown",
     (event) => {
      
